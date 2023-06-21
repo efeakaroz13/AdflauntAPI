31f9ea5e0256403821aaede795a7401a434e3dac
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2023-06-22 02:07:14 +0300

    auto review writer 5 stars

---
+++ autoReview.py
@@ -0,0 +1,99 @@
+import pymongo
+import random
+import json
+import time
+import datetime
+
+sampleReviews = ["Excellent service!","What a great service!","Good deal!","Fair Price!"]
+
+client = pymongo.MongoClient()
+db = client["Adflaunt"]
+users = db["Users"]
+listings = db["Listings"]
+bookings = db["Bookings"]
+
+
+allUsers = users.find({})
+for user in allUsers:
+        allUsersListings= listings.find({"user":user["_id"]})
+        for listingData in allUsersListings:
+            listingID = listingData["_id"]
+            allBookings = []
+            for b in bookings.find({"_id":listingID})[0]["activeOrders"]:
+                lastDay = b["daysWantToBook"][-1]
+                year = int(lastDay.split("-")[0])
+                month = int(lastDay.split("-")[1])
+                day = int(lastDay.split("-")[2])
+                lastTimestamp = datetime.datetime(year,month,day,0,0).timestamp()
+                if lastTimestamp+259200<time.time():
+                    allBookings.append(b["bookingID"])
+            for bookingID in allBookings:
+
+
+        
+                review = random.choice(sampleReviews)
+                stars = 5.0
+
+                
+
+
+                try:
+                    bookingData = bookings.find({"_id": listingID})[0]
+                except:
+                    return {"SCC": False, "err": "Could not find booking data"}
+                activeOrders = bookingData["activeOrders"]
+                current = None
+                for a in activeOrders:
+                    if bookingID == a["bookingID"]:
+                        current = bookingID
+
+                        break
+                if current == None:
+                    return {"SCC": False, "err": "Could not find requested booking"}
+
+                bookingData["doneOrders"].append(current)
+                cindex = bookingData["activeOrders"].index(current)
+                bookingData["activeOrders"].pop(cindex)
+
+                bookings.update_one({"_id": listingID}, {"$set": {bookingData}})
+
+                try:
+                    hostProfile = users.find({"_id": listingData["user"]})[0]
+                except:
+                    return {"SCC": False, "err": "Could not find host's profile, instead made the booking DONE."}
+                del user["password"]
+
+                reviewdata = {
+                    "customer": user,
+                    "at": time.time(),
+                    "review": review,
+                    "star": stars,
+                    "host": listingData["user"],
+                    "listing": listingID,
+                    "revenue": current["price"] * (100 - commisionRate / 100),
+                }
+                try:
+                    hostProfile["reviews"]
+                except:
+                    hostProfile["reviews"] = []
+                hostProfile["reviews"].append(reviewdata)
+                users.update_one({"_id": listingData["user"]}, {"$set": {"reviews": hostProfile["reviews"]}})
+                try:
+                    balance = hostProfile["balance"]
+                except:
+                    balance = 0
+                revenue = current["price"] * (100 - commisionRate / 100)
+                balance = balance + revenue
+                users.update_one({"_id": listingData["user"]}, {"$set": {"balance": balance}})
+
+                try:
+                    listingData["reviews"]
+                except:
+                    listingData["reviews"] = []
+
+                listingData["reviews"].append(reviewdata)
+                listings.update({"_id": listingID}, {"$set": {"reviews": listingData["reviews"]}})
+
+                reviewdata["SCC"] = True
+                print(json.dumps(reviewdata,indent=4))
+
